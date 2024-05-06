5ec6343a9f1620e4b193e83813df133f017e755e
arisu <318013612@qq.com>
2024-05-06 22:14:44 +0800

    night

--- model/article.go
+++ model/article.go
@@ -21,5 +21,16 @@ func GetArticleById(id uint) (*Article, error) {
 
 // 将文章插入db与es
 func InsertArticle(title string, content string) {
+	// 1.插入mysql，返回主键id
 
+	// 2.插入es
+}
+
+// 模糊搜索
+func QueryArticleByKeyword(keyword string) {
+	// 查询es，获取ids
+
+	// 通过ids查询Mysql，获得数据
+
+	// 返回
 }

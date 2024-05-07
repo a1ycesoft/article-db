b8c76cf5bfda901614b429170b9e8c8b929525a4
arisu <318013612@qq.com>
2024-05-07 19:50:34 +0800

    批量导入完毕

--- client/crawler.go
+++ client/crawler.go
@@ -4,19 +4,24 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
-	"trpc.group/trpc-go/trpc-go/log"
 )
 
 var cnt = 1
 
 func main() {
-	file, err := os.OpenFile("./client/a.txt", os.O_RDONLY, 0)
+	//load1("./client/a.txt")
+	//load2("./client/c.txt")
+	//load3("./client/d.txt")
+	load4("./client/e.txt")
+}
+
+func load1(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -29,7 +34,7 @@ func main() {
 		if lineData, err := reader.ReadString('\n'); err != nil {
 			if err == io.EOF {
 				fmt.Println(cnt, " ", t)
-				send1(t, s)
+				showTitleAndContent(t, s)
 				break
 			}
 		} else {
@@ -38,13 +43,13 @@ func main() {
 				if t != "" {
 					fmt.Println(cnt, " ", t)
 					cnt++
-					send1(t, s)
+					showTitleAndContent(t, s)
 					s = ""
 					//time.Sleep(time.Second)
 				}
 				lineData = strings.TrimRight(lineData, "\r\n")
 				split := strings.Split(lineData, " ")
-				fmt.Println(split)
+				//fmt.Println(split)
 				t = split[1]
 				continue
 			} else {
@@ -55,9 +60,130 @@ func main() {
 		}
 	}
 }
-func send1(title string, content string) {
-	//fmt.Println(title)
-	//fmt.Println(content)
+func load2(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var s string
+	var t string
+	for {
+		if lineData, err := reader.ReadString('\n'); err != nil {
+			if err == io.EOF {
+				fmt.Println(cnt, " ", t)
+				send(t, s)
+				break
+			}
+		} else {
+			runes := []rune(lineData)
+			if runes[0] == '正' {
+				if t != "" {
+					fmt.Println(cnt, " ", t)
+					cnt++
+					send(t, s)
+					s = ""
+					//time.Sleep(time.Second)
+				}
+				lineData = strings.TrimRight(lineData, "\r\n")
+				split := strings.Split(lineData, " ")
+				//fmt.Println(split)
+				t = split[2]
+				continue
+			} else {
+				now := strings.TrimRight(lineData, "\r\n")
+				s += now
+			}
+
+		}
+	}
+}
+func load3(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var s string
+	var t string
+	for {
+		if lineData, err := reader.ReadString('\n'); err != nil {
+			if err == io.EOF {
+				fmt.Println(cnt, " ", t)
+				send(t, s)
+				break
+			}
+		} else {
+			runes := []rune(lineData)
+			if runes[0] == '第' {
+				if t != "" {
+					fmt.Println(cnt, " ", t)
+					cnt++
+					send(t, s)
+					s = ""
+					//time.Sleep(time.Second)
+				}
+				lineData = strings.TrimRight(lineData, "\r\n")
+				split := strings.Split(lineData, " ")
+				//fmt.Println(split)
+				t = split[1]
+				continue
+			} else {
+				now := strings.TrimRight(lineData, "\r\n")
+				s += now
+			}
+
+		}
+	}
+}
+func load4(filename string) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var s string
+	var t string
+	for {
+		if lineData, err := reader.ReadString('\n'); err != nil {
+			if err == io.EOF {
+				fmt.Println(cnt, " ", t)
+				send(t, s)
+				break
+			}
+		} else {
+			runes := []rune(lineData)
+			if runes[0] == '第' {
+				if t != "" {
+					fmt.Println(cnt, " ", t)
+					cnt++
+					send(t, s)
+					s = ""
+					//time.Sleep(time.Second)
+				}
+				lineData = strings.TrimRight(lineData, "\r\n")
+				split := strings.Split(lineData, " ")
+				//fmt.Println(split)
+				t = split[1]
+				continue
+			} else {
+				now := strings.TrimRight(lineData, "\r\n")
+				s += now
+			}
+
+		}
+	}
+}
+
+func showTitleAndContent(title string, content string) {
+	fmt.Println(title)
+	fmt.Println(content)
 }
 
 func send(title string, content string) {
@@ -84,35 +210,35 @@ func send(title string, content string) {
 	fmt.Println(string(body))
 }
 
-func hitCrawl(url string) (string, string) {
-	// 请求html页面
-	res, err := http.Get(url)
-	if err != nil {
-		// 错误处理
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-	// 加载 HTML document对象
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var t, c string
-	// Find the review items
-	doc.Find(".article_title").Each(func(i int, s *goquery.Selection) {
-		t = s.Text()
-		return
-	})
-	doc.Find(".wp_articlecontent").Each(func(i int, s *goquery.Selection) {
-		c = s.Text()
-		return
-	})
-	c = strings.ReplaceAll(c, "\n", "")
-	c = strings.ReplaceAll(c, " ", "")
-	fmt.Println("title:", t)
-	fmt.Println("content:", c)
-	return t, c
-}
+//func hitCrawl(url string) (string, string) {
+//	// 请求html页面
+//	res, err := http.Get(url)
+//	if err != nil {
+//		// 错误处理
+//		log.Fatal(err)
+//	}
+//	defer res.Body.Close()
+//	if res.StatusCode != 200 {
+//		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+//	}
+//	// 加载 HTML document对象
+//	doc, err := goquery.NewDocumentFromReader(res.Body)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	var t, c string
+//	// Find the review items
+//	doc.Find(".article_title").Each(func(i int, s *goquery.Selection) {
+//		t = s.Text()
+//		return
+//	})
+//	doc.Find(".wp_articlecontent").Each(func(i int, s *goquery.Selection) {
+//		c = s.Text()
+//		return
+//	})
+//	c = strings.ReplaceAll(c, "\n", "")
+//	c = strings.ReplaceAll(c, " ", "")
+//	fmt.Println("title:", t)
+//	fmt.Println("content:", c)
+//	return t, c
+//}
